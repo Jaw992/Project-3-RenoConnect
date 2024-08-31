02d39f6837bf343254e1752dd29adab895b14d3a
Jared Aw <jaw@Jareds-MacBook-Air.local>
2024-08-31 15:59:55 +0800

    feat: added contractor login

--- backend/controllers/ContractorsController.js
+++ backend/controllers/ContractorsController.js
@@ -19,15 +19,35 @@ const createJWT = (contractor) => {
 router.post("/signup", async (req, res) => {
     const { username, name, contact, email, company, companyUEN } = req.body;
     try {
-        const contractorInDatabase = await Contractor.findOne({Contractor});
+        const contractorInDatabase = await Contractor.findOne({username});
         if (contractorInDatabase) {
             return res.status(400).json({ error: "Username Taken."});
         }
 
         const hashedPassword = await bcrypt.hash(req.body.password, SALT_LENGTH);
-        const contractor = await Contractor.create({ username, hashedPassword, name, contact, email, company, companyUEN});
-        const token = createJWT(contractor);
-        res.status(201).json({contractor, token});
+        const contractorUser = await Contractor.create({ username, hashedPassword, name, contact, email, company, companyUEN});
+        const token = createJWT(contractorUser);
+        res.status(201).json({contractorUser, token});
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+router.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const contractorUser = await Contractor.findOne({ username });
+        if (!contractorUser) {
+            return res.status(401).json({ error: "No such User"});
+        }
+
+        const match = await bcrypt.compare(password, contractorUser.hashedPassword);
+        if (match) {
+            const token = createJWT(contractorUser);
+            return res.status(200).json({ token });
+        }
+        res.status(401).json({ error: "Wrong Password!"});
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
