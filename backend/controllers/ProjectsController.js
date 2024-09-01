7feb9575a4a29f3792718fbe4edc05efa92e14b9
Jared Aw <jaw@Jareds-Air.singtel.com>
2024-09-01 16:18:55 +0800

    feat: added project put for update

--- backend/controllers/ProjectsController.js
+++ backend/controllers/ProjectsController.js
@@ -19,7 +19,7 @@ router.post("/", async (req, res) => {
     }
 });
 
-// //* Get a single project on contractor and customer respectively
+// //* Get a single project
 router.get("/:projectId", async (req, res) => {
     const { projectId } = req.params;
 
@@ -31,6 +31,25 @@ router.get("/:projectId", async (req, res) => {
           res.status(200).json({project});
     } catch (error) {
         res.status(500).json({ error: error.message });
+    }
+});
+
+router.put("/:projectId", async (req, res) => {
+    const { projectId } = req.params;
+    try{
+        const updatedProject = await Project.findByIdAndUpdate(
+            projectId,
+            req.body,
+            { new: true },
+        );
+
+        if (!updatedProject) {
+            return res.status(404).json({ error: 'Project not found' });
+        }
+
+        res.status(200).json(updatedProject);
+    } catch (error) {
+        res.status(500).json(error);
     }
 });
 
