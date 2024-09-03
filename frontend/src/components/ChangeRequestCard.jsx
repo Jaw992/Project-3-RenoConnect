6252467c2a90f9b14289bdbf53c3cd5165570981
Jared Aw <jaw@Jareds-MacBook-Air.local>
2024-09-03 11:00:27 +0800

    feat: style changerequestcard

--- frontend/src/components/ChangeRequestCard.jsx
+++ frontend/src/components/ChangeRequestCard.jsx
@@ -1,13 +1,33 @@
 // import { useState } from "react";
+import { Card } from "react-bootstrap";
 
 const ChangeRequestCard = () => {
 
     return (
       <div>
-        <h5>Phase 1</h5>
-        <strong>Task Description</strong>
-        <p>Before: paint living room</p>
-        <p>After: paint masterbed room</p>
+        <Card
+        className="mb-3"
+        style={{
+          backgroundColor: "transparent",
+          color: "white",
+          border: "none",
+        }}
+      >
+        <Card.Body>
+          <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
+            Phase 1
+          </Card.Text>
+          <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
+            Task Description
+          </Card.Text>
+          <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
+            Before: paint living room
+          </Card.Text>
+          <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
+            Before: paint masterbed room
+          </Card.Text>
+        </Card.Body>
+      </Card>
       </div>
     );
   };
