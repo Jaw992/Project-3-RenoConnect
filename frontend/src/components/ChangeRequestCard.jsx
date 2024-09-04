76fd271113d3c863cf84621611f899abe7ce7cb4
Jared Aw <jaw@Jareds-Air.singtel.com>
2024-09-04 08:28:06 +0800

    feat: added details to change request card

--- frontend/src/components/ChangeRequestCard.jsx
+++ frontend/src/components/ChangeRequestCard.jsx
@@ -1,11 +1,60 @@
-// import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Card } from "react-bootstrap";
+import { getChangeLog } from "../services/apiPhase";
 
-const ChangeRequestCard = () => {
+// const ChangeRequestCard = () => {
 
-    return (
-      <div>
-        <Card
+//     return (
+//       <div>
+//         <Card
+//         className="mb-3"
+//         style={{
+//           backgroundColor: "transparent",
+//           color: "white",
+//           border: "none",
+//         }}
+//       >
+//         <Card.Body>
+//           <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
+//             <strong>Phase 1</strong>
+//           </Card.Text>
+//           <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
+//             Task Description
+//           </Card.Text>
+//           <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
+//             Before: paint living room
+//           </Card.Text>
+//           <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
+//             After: paint masterbed room
+//           </Card.Text>
+//         </Card.Body>
+//       </Card>
+//       </div>
+//     );
+//   };
+
+const ChangeRequestCard = ({ phase, token }) => {
+  const [changeLog, setChangeLog] = useState([]);
+
+  useEffect(() => {
+    const loadChangeLog = async () => {
+        const changeLogData = await getChangeLog(token);
+        console.log("At RequestCard", phase);
+        console.log("CL data:", changeLogData);
+        if (Array.isArray(changeLogData)) {
+          setChangeLog(changeLogData);
+          if (changeLogData.length > 0) {
+            setChangeLog(changeLogData[0]._id);
+          }
+        } 
+    };
+
+    loadChangeLog();
+  }, [phase,token]);
+
+  return (
+    <div>
+      <Card
         className="mb-3"
         style={{
           backgroundColor: "transparent",
@@ -15,21 +64,48 @@ const ChangeRequestCard = () => {
       >
         <Card.Body>
           <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
-            <strong>Phase 1</strong>
+            <strong>{phase.phaseName || "No Phase"}</strong>
           </Card.Text>
           <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
             Task Description
           </Card.Text>
           <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
-            Before: paint living room
+            Before: {changeLog.oldTaskDescription || "N/A"}
           </Card.Text>
           <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
-            After: paint masterbed room
+            After: {changeLog.newTaskDescription || "N/A"}
+          </Card.Text>
+          <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
+            Start Date
+          </Card.Text>
+          <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
+            Before: {changeLog.oldStartDate || "N/A"}
+          </Card.Text>
+          <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
+            After: {changeLog.newStartDate|| "N/A"}
+          </Card.Text>
+          <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
+            End Date
+          </Card.Text>
+          <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
+            Before: {changeLog.oldEndDate || "N/A"}
+          </Card.Text>
+          <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
+            After: {changeLog.newEndDate || "N/A"}
+          </Card.Text>
+          <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
+            Cost
+          </Card.Text>
+          <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
+            Before: {changeLog.oldCost || "N/A"}
+          </Card.Text>
+          <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
+            After: {changeLog.newCost || "N/A"}
           </Card.Text>
         </Card.Body>
       </Card>
-      </div>
-    );
-  };
-  
-  export default ChangeRequestCard;
+    </div>
+  );
+};
+
+export default ChangeRequestCard;
