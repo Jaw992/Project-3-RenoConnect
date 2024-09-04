4a6501ceabc9d4bd2da9fe994113e8d26b5c1c96
Jared Aw <jaw@Jareds-Air.singtel.com>
2024-09-04 12:48:08 +0800

    feat: change request card to get details

--- frontend/src/components/ChangeRequestCard.jsx
+++ frontend/src/components/ChangeRequestCard.jsx
@@ -33,24 +33,27 @@ import { getChangeLog } from "../services/apiPhase";
 //     );
 //   };
 
-const ChangeRequestCard = ({ phase, token }) => {
+const ChangeRequestCard = ({ phase,token }) => {
   const [changeLog, setChangeLog] = useState([]);
 
   useEffect(() => {
     const loadChangeLog = async () => {
-        const changeLogData = await getChangeLog(token);
-        console.log("At RequestCard", phase);
-        console.log("CL data:", changeLogData);
-        if (Array.isArray(changeLogData)) {
-          setChangeLog(changeLogData);
-          if (changeLogData.length > 0) {
-            setChangeLog(changeLogData[0]._id);
+      if (phase._id) {
+        try {
+          const changeLogData = await getChangeLog(phase._id, changeLog._id, token);
+          console.log("At RequestCard", phase);
+          console.log("Change Log Data:", changeLogData);
+          if (changeLogData) {
+            setChangeLog(changeLogData);
           }
-        } 
+        } catch (error) {
+          console.error("Error fetching change log:", error);
+        }
+      }
     };
 
     loadChangeLog();
-  }, [phase,token]);
+  }, [phase, changeLog._id, token]);
 
   return (
     <div>
@@ -66,42 +69,49 @@ const ChangeRequestCard = ({ phase, token }) => {
           <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
             <strong>{phase.phaseName || "No Phase"}</strong>
           </Card.Text>
-          <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
-            Task Description
-          </Card.Text>
-          <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
-            Before: {changeLog.oldTaskDescription || "N/A"}
-          </Card.Text>
-          <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
-            After: {changeLog.newTaskDescription || "N/A"}
-          </Card.Text>
-          <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
-            Start Date
-          </Card.Text>
-          <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
-            Before: {changeLog.oldStartDate || "N/A"}
-          </Card.Text>
-          <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
-            After: {changeLog.newStartDate|| "N/A"}
-          </Card.Text>
-          <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
-            End Date
-          </Card.Text>
-          <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
-            Before: {changeLog.oldEndDate || "N/A"}
-          </Card.Text>
-          <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
-            After: {changeLog.newEndDate || "N/A"}
-          </Card.Text>
-          <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
-            Cost
-          </Card.Text>
-          <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
-            Before: {changeLog.oldCost || "N/A"}
-          </Card.Text>
-          <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
-            After: {changeLog.newCost || "N/A"}
-          </Card.Text>
+          {changeLog && (
+            <>
+              <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
+                Task Description
+              </Card.Text>
+              <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
+                Before: {changeLog.oldTaskDescription || "N/A"}
+              </Card.Text>
+              <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
+                After: {changeLog.newTaskDescription || "N/A"}
+              </Card.Text>
+
+              <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
+                Start Date
+              </Card.Text>
+              <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
+                Before: {changeLog.oldStartDate || "N/A"}
+              </Card.Text>
+              <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
+                After: {changeLog.newStartDate || "N/A"}
+              </Card.Text>
+
+              <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
+                End Date
+              </Card.Text>
+              <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
+                Before: {changeLog.oldEndDate || "N/A"}
+              </Card.Text>
+              <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
+                After: {changeLog.newEndDate || "N/A"}
+              </Card.Text>
+
+              <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
+                Cost
+              </Card.Text>
+              <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
+                Before: {changeLog.oldCost || "N/A"}
+              </Card.Text>
+              <Card.Text className="mb-1" style={{ fontWeight: "500" }}>
+                After: {changeLog.newCost || "N/A"}
+              </Card.Text>
+            </>
+          )}
         </Card.Body>
       </Card>
     </div>
