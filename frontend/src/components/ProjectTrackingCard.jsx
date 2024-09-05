9af1a05d6a065286db4a1a38e7410ae7cf557df0
Jared Aw <jaw@Jareds-Air.singtel.com>
2024-09-05 22:20:04 +0800

    refactor: remove unwanted codes

--- frontend/src/components/ProjectTrackingCard.jsx
+++ frontend/src/components/ProjectTrackingCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Container, Form, Button, Row, Col } from "react-bootstrap";
 import GanttChart from "./GanttChart";
 
